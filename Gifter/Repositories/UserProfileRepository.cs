31fb0c640572d6c4b6731b2f161f02b247c559e9
Meghan Semrad <mlsemrad753@gmail.com>
2021-04-12 13:11:51 -0500

    worked on add method in userProfileRepo

--- Gifter/Repositories/UserProfileRepository.cs
+++ Gifter/Repositories/UserProfileRepository.cs
@@ -11,6 +11,117 @@ namespace Gifter.Repositories
     {
         public UserProfileRepository(IConfiguration configuration) : base(configuration) { }
 
+
+
+
+
+        //public List<Post> GetAll()
+        //{
+        //    using (var conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (var cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"
+        //                  SELECT Id, Title, Caption, DateCreated, ImageUrl, UserProfileId
+        //                    FROM Post
+        //                ORDER BY DateCreated";
+
+        //            var reader = cmd.ExecuteReader();
+
+        //            var posts = new List<Post>();
+        //            while (reader.Read())
+        //            {
+        //                posts.Add(new Post()
+        //                {
+        //                    Id = DbUtils.GetInt(reader, "Id"),
+        //                    Title = DbUtils.GetString(reader, "Title"),
+        //                    Caption = DbUtils.GetString(reader, "Caption"),
+        //                    DateCreated = DbUtils.GetDateTime(reader, "DateCreated"),
+        //                    ImageUrl = DbUtils.GetString(reader, "ImageUrl"),
+        //                    UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+        //                });
+        //            }
+
+        //            reader.Close();
+
+        //            return posts;
+        //        }
+        //    }
+        //}
+
+
+
+
+
+        //public Post GetById(int id)
+        //{
+        //    using (var conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (var cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"
+        //                  SELECT Title, Caption, DateCreated, ImageUrl, UserProfileId
+        //                    FROM Post
+        //                   WHERE Id = @Id";
+
+        //            DbUtils.AddParameter(cmd, "@Id", id);
+
+        //            var reader = cmd.ExecuteReader();
+
+        //            Post post = null;
+        //            if (reader.Read())
+        //            {
+        //                post = new Post()
+        //                {
+        //                    Id = id,
+        //                    Title = DbUtils.GetString(reader, "Title"),
+        //                    Caption = DbUtils.GetString(reader, "Caption"),
+        //                    DateCreated = DbUtils.GetDateTime(reader, "DateCreated"),
+        //                    ImageUrl = DbUtils.GetString(reader, "ImageUrl"),
+        //                    UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+        //                };
+        //            }
+
+        //            reader.Close();
+
+        //            return post;
+        //        }
+        //    }
+        //}
+
+
+
+
+
+        public void Add(UserProfile userProfile)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO UserProfile (Name, Email, ImageUrl, DateCreated)
+                        OUTPUT INSERTED.ID
+                        VALUES (@Title, @Caption, @DateCreated, @ImageUrl, @UserProfileId)";
+
+                    DbUtils.AddParameter(cmd, "@Name", userProfile.Name);
+                    DbUtils.AddParameter(cmd, "@Email", userProfile.Email);
+                    DbUtils.AddParameter(cmd, "@ImageUrl", userProfile.ImageUrl);
+                    DbUtils.AddParameter(cmd, "@DateCreated", userProfile.DateCreated);
+
+                    userProfile.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+
+
+
+
+
         public void Update(UserProfile userProfile)
         {
             using (var conn = Connection)
