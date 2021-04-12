84537d0251ea5f5534327f40b84dfcaa9d88e7ea
Meghan Semrad <mlsemrad753@gmail.com>
2021-04-12 13:18:04 -0500

    worked on GetAll method in userProfileRepo

--- Gifter/Repositories/UserProfileRepository.cs
+++ Gifter/Repositories/UserProfileRepository.cs
@@ -15,40 +15,39 @@ namespace Gifter.Repositories
 
 
 
-        //public List<Post> GetAll()
-        //{
-        //    using (var conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using (var cmd = conn.CreateCommand())
-        //        {
-        //            cmd.CommandText = @"
-        //                  SELECT Id, Title, Caption, DateCreated, ImageUrl, UserProfileId
-        //                    FROM Post
-        //                ORDER BY DateCreated";
+        public List<UserProfile> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                          SELECT Id, Name, Email, DateCreated, ImageUrl
+                            FROM UserProfile
+                        ORDER BY DateCreated";
 
-        //            var reader = cmd.ExecuteReader();
+                    var reader = cmd.ExecuteReader();
 
-        //            var posts = new List<Post>();
-        //            while (reader.Read())
-        //            {
-        //                posts.Add(new Post()
-        //                {
-        //                    Id = DbUtils.GetInt(reader, "Id"),
-        //                    Title = DbUtils.GetString(reader, "Title"),
-        //                    Caption = DbUtils.GetString(reader, "Caption"),
-        //                    DateCreated = DbUtils.GetDateTime(reader, "DateCreated"),
-        //                    ImageUrl = DbUtils.GetString(reader, "ImageUrl"),
-        //                    UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
-        //                });
-        //            }
+                    var userProfiles = new List<UserProfile>();
+                    while (reader.Read())
+                    {
+                        userProfiles.Add(new UserProfile()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            Name = DbUtils.GetString(reader, "Name"),
+                            Email = DbUtils.GetString(reader, "Email"),
+                            DateCreated = DbUtils.GetDateTime(reader, "DateCreated"),
+                            ImageUrl = DbUtils.GetString(reader, "ImageUrl"),
+                        });
+                    }
 
-        //            reader.Close();
+                    reader.Close();
 
-        //            return posts;
-        //        }
-        //    }
-        //}
+                    return userProfiles;
+                }
+            }
+        }
 
 
 
