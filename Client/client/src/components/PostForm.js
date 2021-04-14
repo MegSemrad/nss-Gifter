27f03eb7b9b7c35efcdc058f284a6db90ec32473
Meghan Semrad <mlsemrad753@gmail.com>
2021-04-14 15:00:36 -0500

    created form for adding new post

---
+++ Client/client/src/components/PostForm.js
@@ -0,0 +1,76 @@
+import React, { useContext, useEffect, useState } from "react";
+import { PostContext } from "../providers/PostProvider";
+import { useHistory } from 'react-router-dom';
+
+const PostForm = () => {
+    const { addPost } = useContext(PostContext)
+
+    const [post, setPost] = useState({
+        title: "",
+        caption: "",
+        imageUrl: "",
+        UserProfileId: 1
+    });
+
+    const history = useHistory();
+
+    const handleControlledInputChange = (event) => {
+        const newPost = { ...post }
+        let selectedVal = event.target.value
+        if (event.target.id.includes("Id")) {
+            selectedVal = parseInt(selectedVal)
+        }
+        newPost[event.target.id] = selectedVal
+        setPost(newPost)
+    }
+
+    const handleClickSavePost = (event) => {
+        event.preventDefault()
+        addPost(post)
+            .then(() => history.push("/posts"))
+    }
+
+    return (
+        <form className="PostForm">
+            <h2 className="PostForm__title">New Post</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" id="title" onChange={handleControlledInputChange}
+                        required autoFocus
+                        className="form-control"
+                        placeholder="Post Title"
+                        value={post.Title} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="caption">Caption:</label>
+                    <input type="text" id="caption" onChange={handleControlledInputChange}
+                        required autoFocus
+                        className="form-control"
+                        placeholder="Caption"
+                        value={post.Caption} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="imageUrl">Image URL:</label>
+                    <input type="text" id="imageUrl" onChange={handleControlledInputChange}
+                        required autoFocus
+                        className="form-control"
+                        placeholder="URL"
+                        value={post.ImageUrl} />
+                </div>
+            </fieldset>
+
+
+            <button className="btn btn-primary"
+                onClick={handleClickSavePost}>
+                Save Post
+          </button>
+        </form>
+    )
+};
+
+export default PostForm;
--- Gifter/Controllers/PostController.cs
+++ Gifter/Controllers/PostController.cs
@@ -50,6 +50,7 @@ namespace Gifter.Controllers
         [HttpPost]
         public IActionResult Post(Post post)
         {
+            post.DateCreated = DateTime.Now;
             _postRepository.Add(post);
             return CreatedAtAction("Get", new { id = post.Id }, post);
         }
